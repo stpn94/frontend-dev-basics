a5a6823dccc3aae3603e33935dbd3030c076b8bf
stpn94 <stpn94@gmail.com>
2021-11-17 11:27:21 +0900

    es6 /ex6

--- javascript-practices/es6/ex05.js
+++ javascript-practices/es6/ex05.js
@@ -1,55 +1,55 @@
 /**
- * Arrow Function
+ * 5. Arrow Function
  */
-
 const power = function (x) {
   return x * x;
 };
 
 let numbers = [1, 2, 3, 4, 5];
-numbers.forEach(function (n) {
-  process.stdout.write(`${n}:${power(n)}\t`);
+numbers.forEach(function (number) {
+  process.stdout.write(`${number}:${power(number)}\t`);
 });
 
 // ex1
-console.log("\n-----\n");
-const pow = (x) => x * x;
-numbers.forEach(function (n) {
+console.log("\n----");
+const power2 = (x) => x * x;
+numbers.forEach(function (number) {
   // let result = (function(x){
-  //     return x * x;
-  // })(n);
+  //     return x*x;
+  // })(number);
 
   // let result = (x => {
-  //    return x * x;
-  // })(n);
+  //     return x*x;
+  // })(number);
 
-  let result = ((x) => x * x)(n);
-  process.stdout.write(`${n}:${power(n)}\t`);
+  // let result = (x => x*x)(number);
+  // process.stdout.write(`${number}:${result}\t`);
+
+  // process.stdout.write(`${number}:${(x => x*x)(number)}\t`);
+  process.stdout.write(`${number}:${power(number)}\t`);
 });
 
-// ex2
-console.log("\n-----\n");
+// ex3
+console.log("\n----");
 numbers.forEach((n) => process.stdout.write(`${n}:${((x) => x * x)(n)}\t`));
 
-// ex3 - 여러 행 함수
-console.log("\n-----\n");
+// ex4 - 여러행 함수
+console.log("\n----");
 [5, 3, 15, 1045, 43, 92].forEach((e) => {
   if (e % 5 == 0) {
     process.stdout.write(`${e}\t`);
   }
 });
 
-// ex4 - this를 어휘적 바인딩(Lexical Bind)
-console.log("\n-----\n");
+// ex5: this를 어휘적 바인딩(Lexical Binding)
+console.log("\n----");
 const dooly = {
   name: "둘리",
-  friends: ["또치", "마이콜", "도우너", "길동"],
+  friends: ["또치", "마이콜", "도우너", "길동", "희동이"],
   printFriends: function () {
-    // this.friends.forEach(function(f){
-    //     console.log(`${this.name}의 친구 ${f}`);
-    // }.bind(this));
-
-    this.friends.forEach((f) => console.log(`${this.name}의 친구 ${f}`));
+    this.friends.forEach((friend) =>
+      console.log(`${this.name}의 친구 ${friend}`)
+    );
   },
 };
 
